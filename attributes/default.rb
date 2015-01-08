d269fb8b59c813ac6a73610708b54c9b7f41d276
Ruwan Nawarathne <prnawa@gmail.com>
2015-01-08 11:54:41 +0800

    updated zaproxy_from_binary recipe

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default['zaproxy']['binary']['url'] = 'http://softlayer-sng.dl.sourceforge.net/project/zaproxy/2.3.1/ZAP_2.3.1_Linux.tar.gz'
 default['zaproxy']['binary']['checksum'] = 'D4382891DA3F1AD5B6122C581A44C9ED'
 default['zaproxy']['install_method'] = "binary"
-default['zaproxy']['install_dir'] = "/opt/zaproxy"
+default['zaproxy']['install_dir'] = "/opt/zaproxy-2.3.1"
 default['zaproxy']['host'] = "localhost"
 default['zaproxy']['port'] = "8282"
--- recipes/zaproxy_from_binary.rb
+++ recipes/zaproxy_from_binary.rb
@@ -1,9 +1,10 @@
 zaproxy_bin_url = node['zaproxy']['binary']['url']
-checksum = node['zaproxy']['binary']['checksum']
-install_dir = node['zaproxy']['install_dir']
 
 ark 'zaproxy' do
   url zaproxy_bin_url
-  home_dir install_dir
+  version  '2.3.1'
+  prefix_root '/opt'
+  prefix_home '/opt'
+  prefix_bin  '/opt/bin'
   action :install
 end
